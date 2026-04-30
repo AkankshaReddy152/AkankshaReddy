6fa530789db48e1ae534b13d19b258a7b2a30db6
AkankshaReddy152 <akankshareddy152@gmail.com>
2026-04-30 15:10:50 +0530

    password

---
+++ password.py
@@ -0,0 +1,17 @@
+a=input("Enter you are password:")
+up=0
+sm=0
+sp=0
+dg=0
+if length(a)>7:
+    for i in a:
+        if i .isupper():
+        up=up+1
+        elif i.slower():
+            sm=sm+1
+            else:
+                sp=sp+1
+                if up>0 and sm>0 and sp>0 and dg>0
+                print("STRONG")
+                else:
+                    print("weak due to LESS char")
